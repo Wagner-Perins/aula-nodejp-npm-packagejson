0808252cd314127a54f3dfcbed03af0e380a80c3
Wagner Perins <wagnerperins@gmail.com>
2024-06-21 17:00:28 -0300

    primeiro projeto usando node.js

---
+++ index.js
@@ -0,0 +1,11 @@
+//const crypto = require('crypto') //commons js
+
+//import * as crypto from'crypto'  //ESModules
+
+//console.log(crypto.randomUUID())
+
+import * as cowsay from "cowsay"
+
+let saudacao = cowsay.say({ text: 'muuuuu meu povo' });
+
+console.log (saudacao)
